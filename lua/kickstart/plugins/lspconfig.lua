391bf712ec2b4615dbd5e4ca13dc771dba3b77e0
Vincent Schweiger <vincent.schweiger@xolley.de>
2024-10-18 15:45:02 +0200

    fix lspconfig

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -123,15 +123,14 @@ return {
       ---@type table<string, vim.lsp.Config>
       local servers = {
         clangd = {},
-        --erlangls = {},
-        tsserver = {},
         astro = {},
         jdtls = {},
         nil_ls = {},
         pyright = {},
         gopls = {},
         zls = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
+        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
